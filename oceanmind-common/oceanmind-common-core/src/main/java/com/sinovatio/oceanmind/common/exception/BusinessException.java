f0cd153523e049ff600d07d09b148d582f009107
guoyudong <guoyudong13@163.com>
2020-02-13 07:38:29 +0800

    exception

--- oceanmind-common/oceanmind-common-core/src/main/java/com/sinovatio/oceanmind/common/exception/BusinessException.java
+++ oceanmind-common/oceanmind-common-core/src/main/java/com/sinovatio/oceanmind/common/exception/BusinessException.java
@@ -7,5 +7,35 @@ public class BusinessException extends RuntimeException{
      */
     private static final long serialVersionUID = 1L;
 
+    private String errcode;
+
+    public BusinessException(){
+        super();
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public BusinessException(String errcode,String message, Throwable cause) {
+        super(message, cause);
+        this.errcode = errcode;
+    }
+
+    public String getErrcode() {
+        return errcode;
+    }
+
+    public void setErrcode(String errcode) {
+        this.errcode = errcode;
+    }
+
     
+
+
 }
